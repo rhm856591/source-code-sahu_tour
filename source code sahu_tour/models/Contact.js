793e059c5296f7488ea3e45d307fe432790e0646
Rahamtullah sheikh <76489436+rhm856591@users.noreply.github.com>
2022-01-21 12:11:57 +0530

    Add files via upload

---
+++ source code sahu_tour/models/Contact.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const contactSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    subject: {
+        type: String,
+        required: true,
+    },
+    message: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Sahu_Contact2', contactSchema);
+
